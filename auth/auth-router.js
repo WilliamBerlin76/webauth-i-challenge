bafd49f4c4e29e2c4f0ff10cabfc1dd3d8c8b5e6
WilliamBerlin76 <william.berlin76@gmail.com>
2019-11-11 16:34:21 -0600

    can now register and login

---
+++ auth/auth-router.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs');
+
+const router = require('express').Router();
+
+const Users = require('../users/users-model.js');
+
+router.post('/register', (req, res) => {
+    let userInfo = req.body;
+
+    bcrypt.hash(userInfo.password, 12, (err, hashedPassword) => {
+        userInfo.password = hashedPassword;
+
+        Users.add(userInfo)
+            .then(saved => {
+                res.status(201).json(saved);
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            });
+    });
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)){
+                res.status(200).json({ message: `Welcome ${username}`})
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials' })
+            }
+        })
+        .catch(error => {
+            console.log('login error', error);
+            res.status(500).json(error)
+        })
+});
+
+module.exports = router;
